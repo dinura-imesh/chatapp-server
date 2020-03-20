8dee995fa869487abe88be3de42fc54625ac35c5
dinura-imesh <dinuraimesh@gmail.com>
2020-03-20 20:04:13 +0530

    Fix body-parser line

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let server = app.listen(8000, function(){
+    console.log("Server started!");
+});
+
+const io = require("socket.io")(server);
+
+app.get('/', function(req,res){
+    res.send("server is running!");
+});
+
+io.on('connection', function(socket){
+    socket.on('data', function(data){
+        socket.emit('d', {"name" : dinura});
+    });
+});
